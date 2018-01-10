7453f49946d01f4286841ab1bba2024050a25181
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-10 16:31:58 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def sort_array_asc(array)
+  array.sort {|a, b| a<=>b}
+end
 
+def sort_array_desc(array)
+  array.sort {|a, b| b<=>a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length<=>b.length}
+end
+
+def swap_elements(array)
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+end
+
+def find_a(array)
+  array.select {|word| word.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject(0) {|sum,x| sum+x }
+end
+
+def add_s(array)
+  array.each_with_index.collect {|word, position| word + "s"}
+end
